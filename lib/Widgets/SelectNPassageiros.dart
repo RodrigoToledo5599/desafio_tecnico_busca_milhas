6655c1b8688ff53ced85cb742962b83172dc09d3
RodrigoToledo5599 <rodrigotmt89@gmail.com>
2025-02-26 23:00:01 -0300

    finalizando o widget SelectNPassageiros mas acho que o refarei amanha pois não gostei muito

--- lib/Widgets/SelectNPassageiros.dart
+++ lib/Widgets/SelectNPassageiros.dart
@@ -22,6 +22,26 @@ class SelectNPassageiros extends StatefulWidget{
 }
 
 class SelectNPassageirosState extends State<SelectNPassageiros>{
+  String msgAdulto = "N° de adultos: 0";
+  String msgCrianca = "N° de crianças: 0";
+  String msgBebe = "N° de bebes: 0";
+  String warningMsg = "";
+
+  List<int> values = [0,1,2,3,4,5,6,7,8,9,10];
+
+
+  void assuringThereAreMoreAdultsThanBabies(){
+    int Nbabies = int.parse(widget.bebesController.text);
+    int NAdults = int.parse(widget.adultosController.text);
+    if(NAdults < Nbabies){
+      setState((){
+        warningMsg = "não é possível ter mais bebes do que adultos";
+        widget.bebesController.text = (NAdults - 1).toString();
+        msgBebe = "N° de bebes ${NAdults}";
+      });
+    }
+
+  }
 
   @override
   Widget build(BuildContext context){
@@ -29,7 +49,54 @@ class SelectNPassageirosState extends State<SelectNPassageiros>{
         child: SingleChildScrollView(
           child: Column(
               children:[
-                
+                DropdownButton<int>(
+                  hint: Text("${msgAdulto}"),
+                  items: values.map((int value) {
+                    return DropdownMenuItem<int>(
+                      value: value,
+                      child: Text(value.toString()),
+                    );
+                  }).toList(),
+                  onChanged: (value){
+                    setState(() {
+                      widget.adultosController.text = value.toString();
+                      msgAdulto = "N° de adultos: "+value.toString();
+                      // assuringThereAreMoreAdultsThanBabies();
+                    });
+                  },
+                ),
+                DropdownButton<int>(
+                  hint: Text("${msgCrianca}"),
+                  items: values.map((int value) {
+                    return DropdownMenuItem<int>(
+                      value: value,
+                      child: Text(value.toString()),
+                    );
+                  }).toList(),
+                  onChanged: (value){
+                    setState(() {
+                      widget.criancasController.text = value.toString();
+                      msgCrianca = "N° de crianças: "+value.toString();
+                    });
+                  },
+                ),
+                DropdownButton<int>(
+                  hint: Text("${msgBebe}"),
+                  items: values.map((int value) {
+                    return DropdownMenuItem<int>(
+                      value: value,
+                      child: Text(value.toString()),
+                    );
+                  }).toList(),
+                  onChanged: (value){
+                    setState(() {
+                      widget.bebesController.text = value.toString();
+                      msgBebe = "N° de bebes: "+value.toString();
+                      assuringThereAreMoreAdultsThanBabies();
+                    });
+                  },
+                ),
+                Text("${warningMsg}"),
               ]
           ),
         )
