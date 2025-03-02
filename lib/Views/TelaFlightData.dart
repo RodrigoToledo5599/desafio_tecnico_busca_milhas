9a0d62beac9a22423fc1c31c276130f81c308720
RodrigoToledo5599 <rodrigotmt89@gmail.com>
2025-03-02 12:21:01 -0300

    salvando meu progresso com a TelaFlightData

--- lib/Views/TelaFlightData.dart
+++ lib/Views/TelaFlightData.dart
@@ -3,8 +3,29 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:desafio_tecnico_busca_milhas/Widgets/UpBar.dart';
 import 'package:desafio_tecnico_busca_milhas/Models/FlightModel.dart';
-import 'package:desafio_tecnico_busca_milhas/Widgets/TelaInicial/SelectCompanhiaAerea.dart';
 
+
+
+
+class SpaceBetweenRows extends StatelessWidget{
+  double spacebetweenRows = 0.01;
+  @override
+  Widget build(BuildContext context){
+    return SizedBox(
+      height: MediaQuery.sizeOf(context).height * this.spacebetweenRows,
+    );
+  }
+}
+
+class RowSidePadding extends StatelessWidget{
+  double sidePadding = 0.05;
+  @override
+  Widget build(BuildContext context){
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).height * this.sidePadding,
+    );
+  }
+}
 
 
 class TelaFlightData extends StatefulWidget{
@@ -20,6 +41,7 @@ class TelaFlightData extends StatefulWidget{
 
 class TelaFlightDataState extends State<TelaFlightData>{
   TextEditingController textController = TextEditingController();
+  double fontsize = 15;
 
   @override
   Widget build(BuildContext context) {
@@ -30,24 +52,95 @@ class TelaFlightDataState extends State<TelaFlightData>{
         bottomNavigationBar: TelaFlightDataBottomBar(),
         body: Container(
           width: MediaQuery.sizeOf(context).width * 1,
-          child: SingleChildScrollView(
-              child: Container(
-                  color: Colors.green,
-                  height: MediaQuery.sizeOf(context).height * 1,
-                  child:Column(
-                      children:[
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                            children:[
-                              Text("${widget.Flight.Companhia}")
-                            ]
-                        )
-                      ]
-                  )
-              )
-          ),
-        )
+              child:SingleChildScrollView(
+                child:Column(
+                    children:[
+                      SizedBox(
+                        height: MediaQuery.sizeOf(context).height * 0.05,
+                      ),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children:[
+                            RowSidePadding(),
+                            Text(
+                              "${widget.Flight.NumeroVoo}",
+                              style: TextStyle(
+                                  color: Colors.blue,
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: this.fontsize,
+                              ),
+                            ),
+                            RowSidePadding(),
+                          ]
+                      ),
+                      SpaceBetweenRows(),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children:[
+                            RowSidePadding(),
+                            Text(
+                              "Duração:  ${widget.Flight.Duracao}",
+                              style: TextStyle(
+                                color: Colors.blue,
+                                fontWeight: FontWeight.w600,
+                                fontSize: this.fontsize,
+                              ),
+                            ),
+                            RowSidePadding(),
+                          ]
+                      ),
+                      SpaceBetweenRows(),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children:[
+                            RowSidePadding(),
+                            Text(
+                              "Companhia: ",
+                              style: TextStyle(
+                                  color: Colors.blue,
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: this.fontsize,
+                              ),
+                            ),
+                            Text(
+                              "${widget.Flight.Companhia}",
+                              style: TextStyle(
+                                  color: Colors.blue,
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: this.fontsize,
+                              ),
+                            ),
 
+                          ]
+                      ),
+                      SpaceBetweenRows(),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children:[
+                            RowSidePadding(),
+                            Text(
+                              "N° Conexões: ",
+                              style: TextStyle(
+                                  color: Colors.blue,
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: this.fontsize,
+                              ),
+                            ),
+                            Text(
+                              "${widget.Flight.NumeroConexoes}",
+                              style: TextStyle(
+                                  color: Colors.blue,
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: this.fontsize,
+                              ),
+                            ),
+                            RowSidePadding(),
+                          ]
+                      ),
+                    ]
+                ),
+              )
+        )
       ),
     );
   }
