8634d8d94c6ac850dd4994017afd7ee8690e6f77
RodrigoToledo5599 <rodrigotmt89@gmail.com>
2025-05-02 02:19:28 -0300

    deixando no jeito agr

--- lib/Widgets/TelaInicial/PesquisaAeroporto.dart
+++ lib/Widgets/TelaInicial/PesquisaAeroporto.dart
@@ -21,7 +21,7 @@ class PesquisaAeroportoState extends State<PesquisaAeroporto>{
   final _formKey = GlobalKey<FormState>();
   AirPortViewModel airPortVM = new AirPortViewModel();
   List<AirPortModel> airPorts = [];
-     
+
 
   void search(String airport) async {
     List<AirPortModel> temporary = await this.airPortVM.getAirports(airport);
@@ -33,153 +33,139 @@ class PesquisaAeroportoState extends State<PesquisaAeroporto>{
   @override
   Widget build(BuildContext context){
     return  Container(
-        width: MediaQuery.sizeOf(context).width * 1,
-        child: Column(
+      width: MediaQuery.sizeOf(context).width * 1,
+      child: Column(
           children:[
             Form(
-              key: _formKey,
-              child:
-              Column(
-                children:[
-                  Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                key: _formKey,
+                child:
+                Column(
                     children:[
-                      Container(
-                        width: MediaQuery.sizeOf(context).width * 0.5,
-                        child:
-                        TextFormField(
-                          controller: widget.controller,
-                          keyboardType: TextInputType.text,
-                          decoration: InputDecoration(
-                            labelText: widget.label_text,
-                            fillColor: const Color.fromRGBO(245, 247, 249, 1),
-                            filled: true,
-                            enabledBorder: const OutlineInputBorder(
-                              borderSide:  BorderSide(
-                                color:  Color.fromRGBO(245, 247, 249, 1),
-                                width: 2.0,
-                              ),
-                            ),
-                            focusedBorder: const OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Color.fromRGBO(245, 247, 249, 1),
-                                width: 0.0,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      ElevatedButton(
-                          onPressed:(){
-                            search(widget.controller!.text.toUpperCase());
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: const Color.fromRGBO(255, 255, 255, 1),
-                            side: BorderSide.none,
-                            shadowColor: Colors.transparent,
-                            padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 15),
-                            textStyle: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                            shape: const RoundedRectangleBorder(
-                              side: BorderSide(
-                                color:Color.fromRGBO(255, 255, 255, 1),
-                                width: 0.0,
-                              ),
-                            ),
-                          ),
-                          child: const Icon(
-                              Icons.search,
-                              color: Colors.blue,
-                              size: 25,
-                          ),
-                      ),
-                      ElevatedButton(
-                        onPressed:(){
-                          setState(() {
-                            widget.controller!.text = "";
-                            this.airPorts = [];
-                          });
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: const Color.fromRGBO(255, 255, 255, 1),
-                          side: BorderSide.none,
-                          shadowColor: Colors.transparent,
-                          // foregroundColor: Color.fromRGBO(0, 127, 255, 1),
-                          padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 15),
-                          textStyle: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                          shape: const RoundedRectangleBorder(
-                            side:BorderSide(
-                              color:Color.fromRGBO(255, 255, 255, 1),
-                              width: 0.0,
-                            ),
-                          ),
-                        ),
-                        child: const Icon(
-                            Icons.close,
-                            color: Colors.blue,
-                            size: 25,
-                        ),
-                      ),
-                    ]
-                  ),
-                  const SizedBox(height:20),
-                  Container(
-                      child:FutureBuilder<List<AirPortModel>?>(
-                          future: Future.value(this.airPorts),
-                          builder: (context, snapshot){
-                            if (snapshot.connectionState == ConnectionState.waiting) {
-                              return const Center(child: CircularProgressIndicator());
-                            }
-                            if (snapshot.hasError) {
-                              return const Center(child: Text("Error loading Airports"));
-                            }
-                            if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                              return const Center(child: Text(""));
-                            }
-                            this.airPorts = snapshot.data!;
-                            return Container(
-                              height: MediaQuery.sizeOf(context).height * 0.3,
-                              width: MediaQuery.sizeOf(context).width * 0.75,
-                              child:SingleChildScrollView(
-                                child:Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: this.airPorts.map((airport){
-                                    return
-                                    ElevatedButton(onPressed: () =>{
-                                      setState(() {
-                                        this.airPorts = [];
-                                        widget.controller!.text = airport.Iata.toString();
-                                      }),
-                                    },
-                                        style: ElevatedButton.styleFrom(
-                                          backgroundColor: Colors.white,
-                                          shadowColor: Colors.transparent,
-                                          overlayColor: Colors.transparent,
-                                        ),
-                                        child:Text(
-                                          "${airport.Nome} - ${airport.Iata}\n",
-                                          overflow: TextOverflow.ellipsis,
-                                          maxLines: 2,
-                                          softWrap: false,
-                                          style: const  TextStyle(
-                                            color: Colors.blue,
-                                            fontWeight: FontWeight.w500,
-                                            fontSize: 12,
-                                          ),
-                                        )
-                                    );
-                                  }).toList(),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children:[
+                            Container(
+                              width: MediaQuery.sizeOf(context).width * 0.5,
+                              child:
+                              TextFormField(
+                                controller: widget.controller,
+                                keyboardType: TextInputType.text,
+                                decoration: InputDecoration(
+                                  labelText: widget.label_text,
+                                  fillColor: const Color.fromRGBO(245, 247, 249, 1),
+                                  filled: true,
+                                  enabledBorder: const OutlineInputBorder(
+                                    borderSide:  BorderSide(
+                                      color:  Color.fromRGBO(245, 247, 249, 1),
+                                      width: 2.0,
+                                    ),
+                                  ),
+                                  focusedBorder: const OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Color.fromRGBO(245, 247, 249, 1),
+                                      width: 0.0,
+                                    ),
+                                  ),
                                 ),
                               ),
-                            );
-                          }
+                            ),
+                            ElevatedButton(
+                              onPressed:(){
+                                search(widget.controller!.text.toUpperCase());
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: const Color.fromRGBO(255, 255, 255, 1),
+                                side: BorderSide.none,
+                                shadowColor: Colors.transparent,
+                                padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 15),
+                                textStyle: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                                shape: const RoundedRectangleBorder(
+                                  side: BorderSide(
+                                    color:Color.fromRGBO(255, 255, 255, 1),
+                                    width: 0.0,
+                                  ),
+                                ),
+                              ),
+                              child: const Icon(
+                                Icons.search,
+                                color: Colors.blue,
+                                size: 25,
+                              ),
+                            ),
+                            ElevatedButton(
+                              onPressed:(){
+                                setState(() {
+                                  widget.controller!.text = "";
+                                  this.airPorts = [];
+                                });
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: const Color.fromRGBO(255, 255, 255, 1),
+                                side: BorderSide.none,
+                                shadowColor: Colors.transparent,
+                                // foregroundColor: Color.fromRGBO(0, 127, 255, 1),
+                                padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 15),
+                                textStyle: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                                shape: const RoundedRectangleBorder(
+                                  side:BorderSide(
+                                    color: const Color.fromRGBO(255, 255, 255, 1),
+                                    width: 0.0,
+                                  ),
+                                ),
+                              ),
+                              child: const Icon(
+                                Icons.close,
+                                color: Colors.blue,
+                                size: 25,
+                              ),
+                            ),
+                          ]
+                      ),
+                      const SizedBox(height:20),
+                      Container(
+                        child: airPorts.isNotEmpty
+                            ?
+                        SizedBox(
+                          height: MediaQuery.sizeOf(context).height * 0.3,
+                          child: ListView.builder(
+                            itemCount: airPorts.length,
+                            itemBuilder: (context, index) {
+                              final airport = airPorts[index];
+                              return ElevatedButton(
+                                onPressed: () {
+                                  setState(() {
+                                    airPorts = [];
+                                    widget.controller!.text = airport.Iata.toString();
+                                  });
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.white,
+                                  shadowColor: Colors.transparent,
+                                  overlayColor: Colors.transparent,
+                                ),
+                                child: Text(
+                                  "${airport.Nome} - ${airport.Iata}\n",
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 2,
+                                  softWrap: false,
+                                  style: const TextStyle(
+                                    color: Colors.blue,
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 12,
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        )
+                            : const Center(child: Text("")),
                       )
-                  )
-                ]
-              )
+                    ]
+                )
             ),
           ]
-        ),
-      );
+      ),
+    );
   }
 }
 
